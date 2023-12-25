78b3e38667d44826c234b1a4b1288299468de358
amar9971 <rai.amarjeet31@gmail.com>
2023-12-25 23:22:46 +0530

    new project

---
+++ online_electronic_shopping/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OnlineElectronicShoppingConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'online_electronic_shopping'
