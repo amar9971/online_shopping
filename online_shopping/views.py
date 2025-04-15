76e95175aee505fa11502682f7f65fa0e7aac6d4
amar9971 <rai.amarjeet31@gmail.com>
2025-04-15 21:48:11 +0530

    update new

--- online_shopping/views.py
+++ online_shopping/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from online_electronic_shopping.models import Product
+from online_electronic_shopping.models import Product, Categories, Filter_Price, Color,Brand
 
 
 def BASE(request):
@@ -18,8 +18,36 @@ def home(request):
 
 def PRODUCT(request):
     product = Product.objects.filter(status='Publish')
+    categories = Categories.objects.all()
+    filter_price= Filter_Price.objects.all()
+    color = Color.objects.all()
+    brand= Brand.objects.all()
+
+    CATID = request.GET.get('categories')
+    PRICE_FILTER_ID= request.GET.get('filter_Price')
+    COLOR_ID= request.GET.get('Color')
+    BRAND_ID= request.GET.get('brand')
+
+    if CATID:
+        product= Product.objects.filter(Categories=CATID,status='Publish')
+
+    elif PRICE_FILTER_ID:
+        product= Product.objects.filter(Filter_Price='PRICE_FILTER_ID',status='Publish')
+    elif COLOR_ID:
+        product= Product.objects.filter(Color='COLOR_ID',status='Publish')
+
+    elif BRAND_ID:
+        product=Product.objects.filter(Brand='BRAND_ID',status='Publish')
+
+    else:
+        product= Product.objects.filter(status='Publish')
 
     cantext = {
         'product': product,
+        'categories':categories,
+        'filter_price': filter_price,
+        'color': color,
+        'brand':brand,
+
     }
     return render(request,'main/product.html',cantext)
